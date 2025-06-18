750cbab018548e22774c31e65de7745e64383f07
Arthur Coelho <abcoelhojc@gmail.com>
2025-06-18 12:52:14 -0300

    create vaga ocupada

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { VagasModule } from './vagas/vagas.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
+import { VeiculosModule } from './veiculos/veiculos.module';
+import { VagaOcupadaModule } from './vaga-ocupada/vaga-ocupada.module';
 
 @Module({
     imports: [VagasModule, 
-        TypeOrmModule.forRoot(dataSourceOptions)
+        TypeOrmModule.forRoot(dataSourceOptions), VeiculosModule, VagaOcupadaModule
     ],
     controllers: [AppController],
     providers: [AppService],
